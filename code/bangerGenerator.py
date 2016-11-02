0f2158bb8395a665700f0d95a9225a240d6e1c96
Valentin Perez <valpe70@gmail.com>
2016-11-02 14:37:37 -0400

    code folder and first python file

---
+++ code/bangerGenerator.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Import data
+from tensorflow.examples.tutorials.mnist import input_data
+
+#from tqdm import tqdm
+
+import tensorflow as tf
+import numpy as np
+import re
+import operator
+import math
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
